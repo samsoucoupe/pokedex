8dd337fa813055955be0a7b04fd133960c01ed9b
samyBendhiab <samybendhiab06@outlook.fr>
2022-09-22 11:41:19 +0200

    use api and get type

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
 from flask import Flask,render_template
 import link_database as ldb
-import requests
+
 app = Flask(__name__)
 
 
 
 
-class pokemon:
-    def __init__(self,id):
-        self.id = id
-        self.getpokemon = requests.get("https://pokeapi.co/api/v2/pokemon/"+str(self.id)).json()
-        self.name= self.getpokemon["name"]
-        self.type_pokemon = self.getpokemon["types"][0]["type"]["name"]
-        if len(self.getpokemon["types"]) > 1:
-            self.type_pokemon2 = self.getpokemon["types"][1]["type"]["name"]
-        else:
-            self.type_pokemon2 = None
-        self.image = self.getpokemon["sprites"]["front_default"]
-        self.image_shiny = self.getpokemon["sprites"]["front_shiny"]
-        self.hp = self.getpokemon["stats"][0]["base_stat"]
-        self.attack = self.getpokemon["stats"][1]["base_stat"]
-        self.defense = self.getpokemon["stats"][2]["base_stat"]
-        self.speed = self.getpokemon["stats"][5]["base_stat"]
 
-        print(self.type_pokemon)
-    def retourne_les_stats(self):
-        return {"id":id,"name":self.name,"type":self.type_pokemon,"hp":self.hp,"attack":self.attack,"defense":self.defense,"speed":self.speed}
 liste_pokemon = []
-for i in range(10):
-    liste_pokemon.append(pokemon(i+1).retourne_les_stats())
-
-
+ldb.reset()
+ldb.cree_la_table()
+for i in range(1,10):
+    liste_pokemon.append(ldb.pokemon(i))
+    print(liste_pokemon[i-1].name)
+    print(liste_pokemon[i-1].type_pokemon)
+    print(liste_pokemon[i-1].hp)
+    print(liste_pokemon[i-1].attack)
+    print(liste_pokemon[i-1].defense)
+    print(liste_pokemon[i-1].speed)
+    print(liste_pokemon[i-1].image)
+    print(liste_pokemon[i-1].image_shiny)
 @app.route('/')
 def pokedex():  # put application's code here
     return render_template('base.html',pokemons=liste_pokemon)
--- link_database.py
+++ link_database.py
@@ -1,24 +1,51 @@
 from sqlite3 import *
 lien_vers_le_fichier ="database/pokedex.db"
+import requests
+class pokemon:
+    def __init__(self,id):
+        self.id = id
+        self.getpokemon = requests.get("https://pokeapi.co/api/v2/pokemon/"+str(self.id)).json()
+        self.getpokemon_fr=requests.get("https://pokebuildapi.fr/api/v1/pokemon/"+str(self.id)).json()
+        self.name=self.getpokemon_fr["name"]
+        self.type_pokemon=self.getpokemon_fr["apiTypes"]
+        #self.type_pokemon = self.getpokemon["types"][0]["type"]["name"]
+        self.type_pokemon1=self.type_pokemon[0]["name"]
+        if len(self.type_pokemon) > 1:
+            self.type_pokemon2 = self.type_pokemon[1]["name"]
+        else:
+            self.type_pokemon2 = " "
+
+        self.image = self.getpokemon["sprites"]["front_default"]
+        self.image_shiny = self.getpokemon["sprites"]["front_shiny"]
+        self.hp = self.getpokemon["stats"][0]["base_stat"]
+        self.attack = self.getpokemon["stats"][1]["base_stat"]
+        self.defense = self.getpokemon["stats"][2]["base_stat"]
+        self.speed = self.getpokemon["stats"][5]["base_stat"]
+
+        ajouter_un_pokemon(self.id,self.name,self.type_pokemon1,self.type_pokemon2,self.hp,self.attack,self.defense,self.speed,self.image,self.image_shiny)
 #cree la table
 def cree_la_table():
     conn = connect(lien_vers_le_fichier )
     c = conn.cursor()
-    c.execute("CREATE TABLE pokemon (name TEXT, type TEXT, hp INTEGER, attack INTEGER, defense INTEGER, speed INTEGER)")
+    c.execute("CREATE TABLE IF NOT EXISTS pokemon (id INTEGER PRIMARY KEY,name TEXT,type_pokemon1 TEXT,type_pokemon2 TEXT,hp INTEGER,attack INTEGER,defense INTEGER,speed INTEGER,image TEXT,image_shiny TEXT)")
     conn.commit()
-
+#reset la table
+def reset():
+    conn = connect(lien_vers_le_fichier )
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS pokemon")
+    conn.commit()
 def recuper_les_stats_des_pokemon():
     conn = connect(lien_vers_le_fichier )
     c = conn.cursor()
     c.execute("SELECT * FROM pokemon")
     return c.fetchall()
 
-def ajouter_un_pokemon(name,type,hp,attack,defense,speed):
+def ajouter_un_pokemon(id,name,type_pokemon1,type_pokemon2,hp,attack,defense,speed,image,image_shiny):
     conn = connect(lien_vers_le_fichier)
     c = conn.cursor()
-    c.execute("INSERT INTO pokemon VALUES (?,?,?,?,?,?)",(name,type,hp,attack,defense,speed))
+    c.execute("INSERT INTO pokemon VALUES (?,?,?,?,?,?,?,?,?,?)",(id,name,type_pokemon1,type_pokemon2,hp,attack,defense,speed,image,image_shiny))
     conn.commit()
-
 
 def supprimer_un_pokemon(name):
     conn = connect(lien_vers_le_fichier )
