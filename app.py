7035c359b1afcf50e81623593e5b735174677ef4
samyBendhiab <samybendhiab06@outlook.fr>
2022-09-22 13:59:29 +0200

    use api and get type + add type picture

--- app.py
+++ app.py
@@ -7,19 +7,14 @@ app = Flask(__name__)
 
 
 
-liste_pokemon = []
-ldb.reset()
-ldb.cree_la_table()
-for i in range(1,10):
-    liste_pokemon.append(ldb.pokemon(i))
-    print(liste_pokemon[i-1].name)
-    print(liste_pokemon[i-1].type_pokemon)
-    print(liste_pokemon[i-1].hp)
-    print(liste_pokemon[i-1].attack)
-    print(liste_pokemon[i-1].defense)
-    print(liste_pokemon[i-1].speed)
-    print(liste_pokemon[i-1].image)
-    print(liste_pokemon[i-1].image_shiny)
+liste_pokemon=[]
+#ldb.reset()
+#ldb.cree_la_table()
+for i in range(1, 10):
+    ldb.pokemon(i)
+
+print(ldb.recuper_les_stats_des_pokemon())
+liste_pokemon=ldb.recuper_les_stats_des_pokemon()
 @app.route('/')
 def pokedex():  # put application's code here
     return render_template('base.html',pokemons=liste_pokemon)
--- link_database.py
+++ link_database.py
@@ -3,30 +3,37 @@ lien_vers_le_fichier ="database/pokedex.db"
 import requests
 class pokemon:
     def __init__(self,id):
-        self.id = id
-        self.getpokemon = requests.get("https://pokeapi.co/api/v2/pokemon/"+str(self.id)).json()
-        self.getpokemon_fr=requests.get("https://pokebuildapi.fr/api/v1/pokemon/"+str(self.id)).json()
-        self.name=self.getpokemon_fr["name"]
-        self.type_pokemon=self.getpokemon_fr["apiTypes"]
-        #self.type_pokemon = self.getpokemon["types"][0]["type"]["name"]
-        self.type_pokemon1=self.type_pokemon[0]["name"]
-        self.type_pokemon1_assests=self.type_pokemon[0]["image"]
-        if len(self.type_pokemon) > 1:
-            self.type_pokemon2 = self.type_pokemon[1]["name"]
-            self.type_pokemon2_assests=self.type_pokemon[1]["image"]
-        else:
-            self.type_pokemon2 = " "
-            self.type_pokemon2_assests=" "
 
-        self.image = self.getpokemon["sprites"]["front_default"]
-        self.image_shiny = self.getpokemon["sprites"]["front_shiny"]
-        self.hp = self.getpokemon["stats"][0]["base_stat"]
-        self.attack = self.getpokemon["stats"][1]["base_stat"]
-        self.defense = self.getpokemon["stats"][2]["base_stat"]
-        self.speed = self.getpokemon["stats"][5]["base_stat"]
+        if recuper_id(id) == None:
+            self.id = id
+            self.getpokemon = requests.get("https://pokeapi.co/api/v2/pokemon/"+str(self.id)).json()
+            self.getpokemon_fr=requests.get("https://pokebuildapi.fr/api/v1/pokemon/"+str(self.id)).json()
+            self.name=self.getpokemon_fr["name"]
+            self.type_pokemon=self.getpokemon_fr["apiTypes"]
+            #self.type_pokemon = self.getpokemon["types"][0]["type"]["name"]
+            self.type_pokemon1=self.type_pokemon[0]["name"]
+            self.type_pokemon1_assests=self.type_pokemon[0]["image"]
+            if len(self.type_pokemon) > 1:
+                self.type_pokemon2 = self.type_pokemon[1]["name"]
+                self.type_pokemon2_assests=self.type_pokemon[1]["image"]
+            else:
+                self.type_pokemon2 = "⠀"
+                self.type_pokemon2_assests=" "
 
-        ajouter_un_pokemon(self.id,self.name,self.type_pokemon1,self.type_pokemon2,self.type_pokemon1_assests,self.type_pokemon2_assests,self.hp,self.attack,self.defense,self.speed,self.image,self.image_shiny)
-#cree la table
+            self.image = self.getpokemon["sprites"]["front_default"]
+            self.image_shiny = self.getpokemon["sprites"]["front_shiny"]
+            self.hp = self.getpokemon["stats"][0]["base_stat"]
+            self.attack = self.getpokemon["stats"][1]["base_stat"]
+            self.defense = self.getpokemon["stats"][2]["base_stat"]
+            self.speed = self.getpokemon["stats"][5]["base_stat"]
+
+            ajouter_un_pokemon(self.id,self.name,self.type_pokemon1,self.type_pokemon2,self.type_pokemon1_assests,self.type_pokemon2_assests,self.hp,self.attack,self.defense,self.speed,self.image,self.image_shiny)
+
+def recuper_les_stats_des_pokemon():
+    liste_pokemon = []
+    for i in rec_les_stats_des_pokemon():
+        liste_pokemon.append({"id":i[0],"name":i[1],"type_pokemon1":i[2],"type_pokemon2":i[3],"type_pokemon1_assests":i[4],"type_pokemon2_assests":i[5],"hp":i[6],"attack":i[7],"defense":i[8],"speed":i[9],"image":i[10],"image_shiny":i[11]})
+    return liste_pokemon
 def cree_la_table():
     conn = connect(lien_vers_le_fichier )
     c = conn.cursor()
@@ -38,7 +45,7 @@ def reset():
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS pokemon")
     conn.commit()
-def recuper_les_stats_des_pokemon():
+def rec_les_stats_des_pokemon():
     conn = connect(lien_vers_le_fichier )
     c = conn.cursor()
     c.execute("SELECT * FROM pokemon")
@@ -55,3 +62,9 @@ def supprimer_un_pokemon(name):
     c = conn.cursor()
     c.execute("DELETE FROM pokemon WHERE name = ?",(name,))
     conn.commit()
+
+def recuper_id(id):
+    conn = connect(lien_vers_le_fichier )
+    c = conn.cursor()
+    c.execute("SELECT id FROM pokemon WHERE id = ?",(id,))
+    return c.fetchone()
